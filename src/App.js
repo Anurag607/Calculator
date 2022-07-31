ad0042e10a94e617bb15fbe69856849dda9f30ab
Anurag <anurag79002@gmail.com>
2022-07-31 19:41:37 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
 import './style.css';
 
+function Num(props) {
+  return (
+    <span className= "numkey">{props.number}</span>
+  );
+}
+
+function Numkeys() {
+  const keys = ["7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0"];
+  var rows = [];
+  keys.forEach(function (ele) {
+    rows.push(<Num number={ele} />);
+  });
+
+  return (
+  <div className='keypad'>
+    {rows}
+    <span id="del"><img src={process.env.PUBLIC_URL+"images/backspace.svg"}  alt="del" /></span>  
+  </div>
+  )
+}
+
+// function Binary(props) {
+//   return (
+//     <span className= "binarykey">{props.operator}</span>
+//   );
+// }
+
 function App() {
   return (
-    <div>
-      <div>
-        <div class= "output">
-            <section class= "screen" data-state="begin"></section>
-        </div>
-        <div class="binary b1 op">
-            <span id="clear">AC</span>
-            <span id= "pi">∏</span>
-            <span id= "percentage">%</span>
-            <span class= "binarykey">/</span>
-        </div>
-        <div class="binary b2 op result">
-            <span class= "binarykey">*</span>
-            <span class= "binarykey">+</span>
-            <span class= "binarykey">-</span>
-            <span id="result">=</span>
-        </div>
-        <div class= "keypad">
-            <span class= "numkey">7</span>
-            <span class= "numkey">8</span>
-            <span class= "numkey">9</span>
-            <span class= "numkey">4</span>
-            <span class= "numkey">5</span>
-            <span class= "numkey">6</span>
-            <span class= "numkey">1</span>
-            <span class= "numkey">2</span>
-            <span class= "numkey">3</span>
-            <span class= "numkey">.</span>
-            <span class= "numkey">0</span>
-            <span id="del"><img src={process.env.PUBLIC_URL+"images/backspace.svg"}  alt="del" /></span>
-        </div>
+    <div className="app">
+      <div className= "output">
+        <section className= "screen" data-state="begin"></section>
       </div>
+      <div className="binary b1 op">
+        <span id="clear">AC</span>
+        <span id= "pi">∏</span>
+        <span id= "percentage">%</span>
+        <span className= "binarykey">/</span>
+      </div>
+      <div className="binary b2 op result">
+        <span className= "binarykey">*</span>
+        <span className= "binarykey">+</span>
+        <span className= "binarykey">-</span>
+        <span id="result">=</span>
+      </div>
+      <Numkeys />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.querySelector('.calc_body'));
+const root = ReactDOM.createRoot(document.querySelector(".calc-body"));
 root.render(
   <React.StrictMode>
     <App />
